2089258d5a3481fa8d52491d133a63408c6f0841
Fatih ÜNLÜ <unlu.fa@gmail.com>
2021-02-24 18:22:32 +0300

    v-model page

--- src/views/basics/Vmodel.vue
+++ src/views/basics/Vmodel.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>model</div>
+  <div style="padding: 4%">
+    <b-form>
+      <b-form-group id="input-group-2" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+        Your name is {{ name }}
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          :value="name2"
+          @input="updatedName"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+        Your name is {{ name2 }}
+      </b-form-group>
+    </b-form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: '',
+      name2: ''
+    };
+  },
+  methods: {
+      updatedName(value) {
+          console.log('value => ', value);
+          this.name2 = value;
+
+        //   @input="val => name2 = val"
+      }
+  }
+};
 </script>
 
 <style>
